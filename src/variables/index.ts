13931e49a513e7502427636b89927dd681e65dba
dennisrijsdijk <git@dennis.gg>
2024-04-26 22:27:55 +0200

    Remove spoofed variables

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -1,11 +1,9 @@
 import actions from "./actions";
-import numbers from "./numbers";
-import spoofed from "./spoofed";
 import literal from "./literal";
+import numbers from "./numbers";
 
 export default [
     ...actions,
     ...literal,
-    ...numbers,
-    ...spoofed
+    ...numbers
 ];
